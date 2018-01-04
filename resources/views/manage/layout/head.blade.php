6b259152d51f9b0e0112ae7122a83267d2698113
Stijn Kievit <0875013@hr.nl>
2018-01-04 19:41:02 +0100

    init config

--- resources/views/manage/layout/head.blade.php
+++ resources/views/manage/layout/head.blade.php
@@ -7,4 +7,7 @@
     </title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Application assets -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
